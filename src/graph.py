a50c1dbfa2b351a6b699f34cf25c94f3510bebdc
nmontard <nmontard@student.42.fr>
2026-07-27 06:21:37 +0200

    m

--- src/graph.py
+++ src/graph.py
@@ -1,21 +1,21 @@
 from . import Connection, Hub, Drone
 
 class Graph:
-    def __init__(self):
-        self.hubs: list[Hub] = []
+    def __init__(self) -> None:
+        self.hubs: dict[str, Hub] = {}
         self.connections: list[Connection] = []
         self.drones: list[Drone] = []
         self.start: Hub
         self.end: Hub
 
-    def add_hub(self, hub: Hub):
-        self.hubs.append(hub)
+    def add_hub(self, hub: Hub) -> None:
+        self.hubs[hub.name] = hub
 
-    def add_connection(self, connection: Connection):
+    def add_connection(self, connection: Connection) -> None:
         self.connections.append(connection)
 
-    def get_neighbors(self, hub: Hub):
-        result: list[Hub] = []
+    def get_neighbors(self, hub: Hub) -> list[tuple[Hub, Connection]]:
+        result: list[tuple[Hub, Connection]] = []
         for connection in self.connections:
             if connection.hub1 == hub:
                 result.append((connection.hub2, connection))
@@ -23,5 +23,5 @@ class Graph:
                 result.append((connection.hub1, connection))
         return result
 
-    def get_hub(self, name):
+    def get_hub(self, name: str) -> Hub:
         return self.hubs[name]
---
+++ src/pathfinder.py
@@ -0,0 +1,54 @@
+from . import Graph, Hub
+
+class pathfinder:
+    ZONE_COST = {
+        "normal": 1,
+        "priority": 1,
+        "restricted": 2,
+    }
+    def __init__(self, graph: Graph):
+        self.graph: Graph = graph
+    
+    def find_shortest_path_for_one_drone(self):
+        
+        distances_from_start: dict[str, float] = {hub.name: float('inf') for hub in self.graph.hubs}
+        distances_from_start[self.graph.start.name] = 0
+        previous_hub: dict[str, str] = {}
+        unvisited: set[str] = {hub.name for hub in self.graph.hubs}
+
+        while unvisited:
+            current_name = self.find_closest_unvisited(distances_from_start, unvisited)
+            if current_name is None:
+                break
+            if current_name == self.graph.end.name:
+                return self.recreate_path(previous_hub, self.graph.start.name, self.graph.end.name)
+            
+            unvisited.remove(current_name)
+            current_hub = self.graph.get_hub(current_name)
+            
+            for neighbor in self.graph.get_neighbors(current_hub):
+                if neighbor.zone_type == "blocked":
+                    continue
+                if neighbor.name not in unvisited:
+                    continue
+                cost = self.ZONE_COST[neighbor.zone_type]
+                new_dist = distances_from_start[current_name] + cost
+
+                if new_dist < distances_from_start[neighbor.name]:
+                    distances_from_start[neighbor.name] = new_dist
+                    previous_hub[neighbor.name] = current_name
+        return None
+    
+    def find_closest_unvisited(self, distances: dict[str, float], unvisited: set[str]) -> str|None:
+        if not unvisited:
+            return None
+        return min(unvisited, key=lambda name: distances[name])
+    
+    def recreate_path(self, previous: dict[str, str], start_name: str, end_name: str) -> list[Hub]:
+        path_names = [end_name]
+        current = end_name
+        while current != start_name:
+            current = previous[current]
+            path_names.append(current)
+        path_names.reverse()
+        return [self.graph.get_hub(name) for name in path_names]
